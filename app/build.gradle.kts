d5b93663326bd30814102e6502a7072906ed5229
Adnanh444 <adnanha6986@gmail.com>
2026-08-29 14:20:00 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.navigation:navigation-compose:2.7.7")
 
     // CameraX
     val cameraxVersion = "1.3.1"
